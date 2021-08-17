eeaecbfca6764abe9f95896a63d617da957a7de8
lmarino98 <lmarino2021@gmail.com>
2021-08-16 23:01:17 -0400

    Cambios en Operadora registro

--- depilarteFE/src/app/pages/registrar/operadorRegistro/operadora.component.ts
+++ depilarteFE/src/app/pages/registrar/operadorRegistro/operadora.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AppModule } from '../../../app.module';
+
+
+
+interface Operadora {
+    value: string;
+    viewValue: string;
+  }
+  interface Pago {
+    value: string;
+    viewValue: string;
+  }
+  interface Tratamiento {
+    value: string;
+    viewValue: string;
+  }
 @Component({
     selector: 'user-cmp',
     moduleId: module.id,
@@ -10,7 +25,22 @@ export class OperadoraComponent implements OnInit{
     private appModule: AppModule;
     ngOnInit(){
     }
+    operadora: Operadora[] = [
+        {value: 'operadora-0', viewValue: 'Maria'},
+        {value: 'operadora-1', viewValue: 'Anna'},
+        {value: 'operadora-2', viewValue: 'Juan'}
+      ];
 
+      pago: Pago[] = [
+        {value: 'pago-0', viewValue: 'Efectivo'},
+        {value: 'pago-1', viewValue: 'Zelle'},
+        {value: 'pago-2', viewValue: 'Bolivares'}
+      ];
+      tratamiento: Tratamiento[] = [
+        {value: 'tratamiento-0', viewValue: 'Bozo'},
+        {value: 'tratamiento-1', viewValue: 'Barbilla'},
+        {value: 'tratamiento-2', viewValue: 'Cuello'}
+      ];
     uploadFile(event){
         const file= event.target.files[0];
         const dir = './../../../assets/img';
