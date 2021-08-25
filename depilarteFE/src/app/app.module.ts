bb18d17a516658376b6a39a62f666f253c404d8d
Juan <juanpablozabala21@gmail.com>
2021-08-24 20:42:58 -0400

    Detalle del Front
    Modificaciones

--- depilarteFE/src/app/app.module.ts
+++ depilarteFE/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
-
 import { SidebarModule } from './sidebar/sidebar.module';
 import { FooterModule } from './shared/footer/footer.module';
 import { NavbarModule} from './shared/navbar/navbar.module';
 import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
-
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
-
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { FormsModule } from '@angular/forms';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -37,16 +34,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     ToastrModule.forRoot(),
     FooterModule,
     FixedPluginModule,
-    FormsModule
+    FormsModule,
   ],
   exports: [
     MatFormFieldModule,
-    MatDatepickerModule,
-    MatInputModule
+    MatInputModule,
   ],
   providers: [
     MatDatepickerModule,
-    MatInputModule
+    MatInputModule,
   ],
   bootstrap: [AppComponent]
 })
