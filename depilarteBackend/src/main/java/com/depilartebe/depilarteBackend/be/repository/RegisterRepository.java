d856d25e3be2893169107964b1d5d9e6130999b3
Juan <juanpablozabala21@gmail.com>
2021-09-04 11:27:19 -0400

    Nuevas tablas

--- depilarteBackend/src/main/java/com/depilartebe/depilarteBackend/be/repository/RegisterRepository.java
+++ depilarteBackend/src/main/java/com/depilartebe/depilarteBackend/be/repository/RegisterRepository.java
@@ -2,8 +2,10 @@ package com.depilartebe.depilarteBackend.be.repository;
 
 import com.depilartebe.depilarteBackend.be.entities.Register;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
@@ -11,6 +13,9 @@ public interface RegisterRepository extends JpaRepository<Register, Long> {
 
     Register findById(long id);
 
+    String FIND_REGISTERS = "";
+    @Query(nativeQuery = true, value = FIND_REGISTERS)
+    List<Register> 
 
 
 }
--- depilartebd.sql
+++ depilartebd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-08-2021 a las 03:07:07
+-- Tiempo de generación: 04-09-2021 a las 17:26:48
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.8
 
@@ -32,6 +32,39 @@ CREATE TABLE `depilarte_metodopagos` (
   `metodoPago` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `depilarte_metodopagos`
+--
+
+INSERT INTO `depilarte_metodopagos` (`id_pagos`, `metodoPago`) VALUES
+(3, 'Efectivo'),
+(2, 'Pago Móvil '),
+(1, 'Zelle');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `depilarte_productos`
+--
+
+CREATE TABLE `depilarte_productos` (
+  `id_productos` int(11) NOT NULL,
+  `id_tratamientos` int(11) NOT NULL,
+  `Proveedor` varchar(100) NOT NULL,
+  `Cantidad` varchar(100) NOT NULL,
+  `Especialista` int(11) NOT NULL,
+  `Precio` varchar(159) NOT NULL,
+  `Descripcion` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `depilarte_productos`
+--
+
+INSERT INTO `depilarte_productos` (`id_productos`, `id_tratamientos`, `Proveedor`, `Cantidad`, `Especialista`, `Precio`, `Descripcion`) VALUES
+(1, 5, 'Productive', '3', 1, '45', NULL),
+(2, 5, 'probandola', '3', 1, '45', 'Pagala');
+
 -- --------------------------------------------------------
 
 --
@@ -48,23 +81,67 @@ CREATE TABLE `depilarte_registros` (
   `Correo` varchar(150) NOT NULL,
   `FechaCumple` date NOT NULL,
   `Direccion` varchar(500) NOT NULL,
-  `Tratamiento` int(11) NOT NULL,
-  `TipoTratamiento` int(11) NOT NULL,
-  `ZonaTratamiento` int(11) NOT NULL,
-  `CantidadSesiones` varchar(5) NOT NULL,
-  `Asistencia` varchar(100) NOT NULL,
-  `ProductoUtilizado` int(11) NOT NULL,
-  `DisparosAntes` varchar(100) NOT NULL,
-  `DisparosDespues` varchar(100) NOT NULL,
-  `DiferenciaDisparos` varchar(100) NOT NULL,
-  `Operadora` int(11) NOT NULL,
-  `Doctora` int(11) NOT NULL,
+  `Tratamiento` int(11) DEFAULT NULL,
+  `TipoTratamiento` int(11) DEFAULT NULL,
+  `ZonaTratamiento` int(11) DEFAULT NULL,
+  `CantidadSesiones` varchar(5) DEFAULT NULL,
+  `Asistencia` varchar(100) DEFAULT NULL,
+  `ProductoUtilizado` int(11) DEFAULT NULL,
+  `DisparosAntes` varchar(100) DEFAULT NULL,
+  `DisparosDespues` varchar(100) DEFAULT NULL,
+  `DiferenciaDisparos` varchar(100) DEFAULT NULL,
+  `Operadora` int(11) DEFAULT NULL,
+  `Doctora` int(11) DEFAULT NULL,
   `FormaPago` int(11) NOT NULL,
   `Abonado` varchar(100) NOT NULL,
   `PrecioTotal` varchar(100) NOT NULL,
   `Comision` varchar(100) NOT NULL,
-  `Nota` varchar(600) NOT NULL
+  `Nota` varchar(600) NOT NULL,
+  `Telefono` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `depilarte_tipotratamientos`
+--
+
+CREATE TABLE `depilarte_tipotratamientos` (
+  `id_tipo` int(11) NOT NULL,
+  `id_tratamientos` int(11) NOT NULL,
+  `nombreTipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `depilarte_tipotratamientos`
+--
+
+INSERT INTO `depilarte_tipotratamientos` (`id_tipo`, `id_tratamientos`, `nombreTipo`) VALUES
+(9, 5, 'Laser'),
+(10, 5, '4D');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `depilarte_tratamientos`
+--
+
+CREATE TABLE `depilarte_tratamientos` (
+  `id_tratamientos` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Especialista` int(11) NOT NULL,
+  `CantidadSesiones` varchar(10) NOT NULL,
+  `PrecioTratamiento` varchar(10) NOT NULL,
+  `ComisionOperadora` varchar(10) NOT NULL,
+  `DescripcionTratamiento` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `depilarte_tratamientos`
+--
+
+INSERT INTO `depilarte_tratamientos` (`id_tratamientos`, `Nombre`, `Especialista`, `CantidadSesiones`, `PrecioTratamiento`, `ComisionOperadora`, `DescripcionTratamiento`) VALUES
+(5, 'Depilación Laser', 1, '5', '300', '2', 'Alta calidad');
 
 -- --------------------------------------------------------
 
@@ -80,8 +157,31 @@ CREATE TABLE `depilarte_usuarios` (
   `Direccion` varchar(300) NOT NULL,
   `Telefono` varchar(15) NOT NULL,
   `Email` varchar(250) NOT NULL,
-  `Cargo` int(11) NOT NULL
+  `Cargo` int(11) NOT NULL,
+  `FechaIngreso` date NOT NULL,
+  `FechaNacimiento` date NOT NULL,
+  `Edad` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `depilarte_zonatratamientos`
+--
+
+CREATE TABLE `depilarte_zonatratamientos` (
+  `id_zona` int(11) NOT NULL,
+  `id_tratamiento` int(11) NOT NULL,
+  `ZonaNombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `depilarte_zonatratamientos`
+--
+
+INSERT INTO `depilarte_zonatratamientos` (`id_zona`, `id_tratamiento`, `ZonaNombre`) VALUES
+(1, 5, 'Piernas'),
+(2, 5, 'Estomago');
 
 --
 -- Índices para tablas volcadas
@@ -95,6 +195,12 @@ ALTER TABLE `depilarte_metodopagos`
   ADD KEY `metodoPago` (`metodoPago`);
 
 --
+-- Indices de la tabla `depilarte_productos`
+--
+ALTER TABLE `depilarte_productos`
+  ADD PRIMARY KEY (`id_productos`);
+
+--
 -- Indices de la tabla `depilarte_registros`
 --
 ALTER TABLE `depilarte_registros`
@@ -102,10 +208,28 @@ ALTER TABLE `depilarte_registros`
   ADD KEY `FormaPago` (`FormaPago`);
 
 --
+-- Indices de la tabla `depilarte_tipotratamientos`
+--
+ALTER TABLE `depilarte_tipotratamientos`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
+-- Indices de la tabla `depilarte_tratamientos`
+--
+ALTER TABLE `depilarte_tratamientos`
+  ADD PRIMARY KEY (`id_tratamientos`);
+
+--
 -- Indices de la tabla `depilarte_usuarios`
 --
 ALTER TABLE `depilarte_usuarios`
   ADD PRIMARY KEY (`id_usuarios`);
+
+--
+-- Indices de la tabla `depilarte_zonatratamientos`
+--
+ALTER TABLE `depilarte_zonatratamientos`
+  ADD PRIMARY KEY (`id_zona`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -115,19 +239,43 @@ ALTER TABLE `depilarte_usuarios`
 -- AUTO_INCREMENT de la tabla `depilarte_metodopagos`
 --
 ALTER TABLE `depilarte_metodopagos`
-  MODIFY `id_pagos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pagos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `depilarte_productos`
+--
+ALTER TABLE `depilarte_productos`
+  MODIFY `id_productos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `depilarte_registros`
 --
 ALTER TABLE `depilarte_registros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `depilarte_tipotratamientos`
+--
+ALTER TABLE `depilarte_tipotratamientos`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `depilarte_tratamientos`
+--
+ALTER TABLE `depilarte_tratamientos`
+  MODIFY `id_tratamientos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `depilarte_usuarios`
 --
 ALTER TABLE `depilarte_usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `depilarte_zonatratamientos`
+--
+ALTER TABLE `depilarte_zonatratamientos`
+  MODIFY `id_zona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
