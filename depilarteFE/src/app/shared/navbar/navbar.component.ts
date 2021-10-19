f0369266c4d9d2e9a5422a13dfdc178c919bf6d4
lmarino98 <lmarino2021@gmail.com>
2021-10-18 20:33:41 -0400

    Historial doc

--- depilarteFE/src/app/shared/navbar/navbar.component.ts
+++ depilarteFE/src/app/shared/navbar/navbar.component.ts
@@ -43,7 +43,7 @@ export class NavbarComponent implements OnInit {
               return this.listTitles[item].title;
           }
       }
-      return 'Principal';
+      return 'Historial';
     }
     sidebarToggle() {
         if (this.sidebarVisible === false) {
